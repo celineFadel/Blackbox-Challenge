2b0a266d70f1f4b28faf644c6c3ad4ee53337403
celinefadel01 <celinegfadel@gmail.com>
2021-01-10 11:30:55 +0200

    HTML/CSS/JS

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const path = require("path");
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("Hello from Render!"));
+app.use("/uploads/", express.static(path.join(__dirname, "/uploads")));
+app.use("/public/", express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => res.sendFile("public/index.html", {root:__dirname}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
